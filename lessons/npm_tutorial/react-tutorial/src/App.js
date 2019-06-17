cdb2f215ad5d178f3092c4377a4a1ddcbd20a400
DrGaster <flammastnguyen@gmail.com>
2019-06-16 20:40:33 -0500

    Central commit

---
+++ lessons/npm_tutorial/react-tutorial/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Button from './components/button';
+
+function App() {
+  return (
+    <div>
+      <p>Hello world!</p>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
